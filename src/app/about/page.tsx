ac86834f4155263d6a38c2e3e7c6715e3a7bb622
Travis Fischer <fisch0920@gmail.com>
2025-07-11 20:23:15 -0700

    🎣

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,9 @@ import { NotionBlock } from './notion-block'
 
 const notion = new NotionAPI()
 
+// cache the notion content for up to 10 minutes
+export const revalidate = 600
+
 export default async function AboutPage() {
   // https://transitive-bs.notion.site/Da-Nang-Hacker-Residency-1feedb27f1248050b005d65de79d151c
   const pageId = '1feedb27f1248050b005d65de79d151c'
